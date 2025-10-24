5d7d5ebe8d5844223ccfe99e319cbc4cef317fbb
nikunj <nikunjkumargarg@gmail.com>
2025-10-25 00:51:47 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,18 @@ const app = express();
 //Order of routes are important...
 //it's like matching the route extactly begines with mentioned one.
 
-app.use("/user",(req,res)=>{
-    req.send("user data");
+app.use("/user/:userId/:noshId",(req,res)=>{
+    console.log(req.query);
+    console.log(req.params);
+    res.send("user data");
 })
 
 app.get("/employee",(req,res)=>{
-    req.send("employee data");
+    res.send("employee data");
 })
 
 app.post("/employee",(req,res)=>{
-    req.send("employee data saved")
+    res.send("employee data saved")
 })
 
 app.listen(3000,()=>{console.log("Application is listening on port no 3000")});

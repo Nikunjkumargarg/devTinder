303294cf1d128d316f2c573fd1272413d984f2aa
nikunj <nikunjkumargarg@gmail.com>
2025-10-25 00:35:17 +0530

    routes using http methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+//Order of routes are important...
+//it's like matching the route extactly begines with mentioned one.
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server")
+app.use("/user",(req,res)=>{
+    req.send("user data");
+})
+
+app.get("/employee",(req,res)=>{
+    req.send("employee data");
+})
+
+app.post("/employee",(req,res)=>{
+    req.send("employee data saved")
 })
 
 app.listen(3000,()=>{console.log("Application is listening on port no 3000")});

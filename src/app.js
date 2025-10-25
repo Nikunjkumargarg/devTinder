cd29a0f2d007d9191d199b04577ecfa319ace0cc
nikunj <nikunjkumargarg@gmail.com>
2025-10-25 07:59:13 +0530

    playing out with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,18 +3,19 @@ const app = express();
 //Order of routes are important...
 //it's like matching the route extactly begines with mentioned one.
 
+
 app.use("/user/:userId/:noshId",(req,res)=>{
     console.log(req.query);
     console.log(req.params);
     res.send("user data");
 })
 
-app.get("/employee",(req,res)=>{
-    res.send("employee data");
-})
+app.get("/employee",[(req,res,next)=>{
+    // res.send("employee data");
+    next();
+},(req,res)=>{
+    res.send("employee data 2")
+}])
 
-app.post("/employee",(req,res)=>{
-    res.send("employee data saved")
-})
 
 app.listen(3000,()=>{console.log("Application is listening on port no 3000")});

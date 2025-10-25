b16a053c39e5ee91d7211b3dd29a5bb3ef71d08f
nikunj <nikunjkumargarg@gmail.com>
2025-10-25 15:14:42 +0530

    playing with middlewares and route handlers

--- src/app.js
+++ src/app.js
@@ -2,20 +2,21 @@ const express = require("express");
 const app = express();
 //Order of routes are important...
 //it's like matching the route extactly begines with mentioned one.
+//request -> middleware chain -> request handler
 
-
-app.use("/user/:userId/:noshId",(req,res)=>{
-    console.log(req.query);
-    console.log(req.params);
-    res.send("user data");
+app.use("/",(req,res,next)=>{
+    next();
 })
 
 app.get("/employee",[(req,res,next)=>{
-    // res.send("employee data");
+    res.send("employee response")
+},(req,res,next)=>{
+    // res.send("employee data 2")
     next();
-},(req,res)=>{
-    res.send("employee data 2")
 }])
 
+app.get("/employee",(req,res)=>{
+    res.send("employee data 3");
+})
 
 app.listen(3000,()=>{console.log("Application is listening on port no 3000")});

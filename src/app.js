4ecf919ba3780522e16c6a222eee1018eaf9d388
nikunj <nikunjkumargarg@gmail.com>
2025-10-27 11:41:12 +0530

    get api

--- src/app.js
+++ src/app.js
@@ -4,12 +4,11 @@
     const dbConnect = require("../database");
     const User = require("./modals/user");
 
+    app.use(express.json());
+
     app.post("/user",(req,res)=>{
-        const user = new User({
-            firstname: "John",
-            email: "john@example.com"
-        });
         try {
+            const user = new User(req.body);
             user.save();
         res.send(user);
         } catch (error) {
@@ -18,12 +17,32 @@
         
     });
 
+    app.get("/user", async(req,res)=>{
+        try {
+            const users = await User.find({emailId: req.body.emailId}); 
+            if(users.length === 0){
+                return res.status(404).send("User not found");
+            } else {
+                res.send(users);
+            }
+        } catch (error) {
+            res.status(500).send("something went wrong", error.message);
+        }
+    });
+
+    app.get("/feed", async(req,res)=>{
+        try {
+            const users = await User.find({});
+            res.send(users);
+        } catch (error) {
+            res.status(500).send("something went wrong", error.message);
+        }
+    });
+
     dbConnect().then(()=>{
         console.log("Database connected");
         app.listen(3000,()=>{console.log("Application is listening on port no 3000")});
     }).catch((err)=>{
         console.log(err);
     });
-
-    console.log("Database connection is pending");
     
--- src/modals/user.js
+++ src/modals/user.js
@@ -4,8 +4,24 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    email: {
+    lastname: {
         type: String,
+        required: true
+    },
+    emailId: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
         required: true
     }
 });

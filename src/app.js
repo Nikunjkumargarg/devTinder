eadf03e4141863d4e31ceb2d3c53b2bbddbe5f88
nikunj <nikunjkumargarg@gmail.com>
2025-10-28 22:31:06 +0530

    auth middleware added

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@
     const User = require("./modals/user");
     const {validateSignUpData} = require("./utils/validation");
     const bcrypt = require("bcrypt");
-    const cookieParser = require("cookie-parser");
     const jwt = require("jsonwebtoken");
+    const userAuth = require("./middlewares/auth");
+    const cookieParser = require("cookie-parser");
+
 
     app.use(express.json());
+    app.use(cookieParser());
 
     app.post("/signup", async(req,res)=>{
 
@@ -51,54 +54,10 @@
        }
     });
 
-    app.get("/user/:emailId", async(req,res)=>{
-        try {
-            const users = await User.find({emailId: req.params.emailId}); 
-            if(users.length === 0){
-                return res.status(404).send("User not found");
-            } else {
-                res.send(users);
-            }
-        } catch (error) {
-            res.status(500).send("something went wrong", error.message);
-        }
+    app.get("/profile", userAuth, async(req,res)=>{
+       res.send(req.user);
     });
 
-    app.get("/feed", async(req,res)=>{
-        try {
-            const users = await User.find({});
-            res.send(users);
-        } catch (error) { 
-            res.status(500).send("something went wrong", error.message);
-        }
-    });
-
-    app.delete("/user/:id", async(req,res)=>{
-        try {
-            const users = await User.findByIdAndDelete(req.params.id);
-            res.send(users);
-        } catch (error) {
-            res.status(500).send("something went wrong", error.message);
-        }
-    });
-
-    app.patch("/user", async(req,res)=>{
-        try {
-            const allowedFields = ["password", "age", "gender", "photoUrl", "about", "skills"];
-            const isUpdateAllowed = Object.keys(req.body).every(field => allowedFields.includes(field));
-            if(!isUpdateAllowed){
-                throw new Error("Invalid fields");
-            }
-            if(req.body.skills.length > 10){
-                throw new Error("Skills must be less than 10");
-            }
-            const users = await User.findOneAndUpdate({emailId: req.body.emailId}, req.body, {returnDocument: "after", runValidators: true});
-            res.send(users);
-        } catch (error) {
-            console.log(error)
-            res.status(500).send("something went wrong", error.message);
-        }
-    }); 
 
     dbConnect().then(()=>{
         console.log("Database connected");
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,20 @@
-const authModule = (req,res,next)=>{
-    const authPass = "xyz";
-    const isAdmin = authPass === "xyz";
-    if(!isAdmin){
+const jwt = require("jsonwebtoken");
+const User = require("../modals/user");
+
+const userAuth = async (req,res,next)=>{
+    console.log("comiing")
+    const token = req.cookies?.token;
+    console.log("token", token);
+    if(!token){
         return res.status(401).send("Unauthorized access");
     }
+    const decoded = jwt.verify(token, "secret");
+    const user = await User.findById(decoded._id);
+    if(!user){
+        return res.status(401).send("Unauthorized access");
+    }
+    req.user = user;
     next();
 }
 
-module.exports = authModule;
+module.exports = userAuth;
